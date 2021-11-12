6f2cdf966f1178fbc1dc1c5a21f3c9b458918285
Myksean Blair <mykseanblair@gmail.com>
2021-11-11 20:27:23 -0600

    For Loops

--- Section02/2.3.7-WhileLoops.js
+++ Section02/2.3.7-WhileLoops.js
@@ -1,11 +1,11 @@
 // ? TO DO: create a while loop that TOTALS the numbers from 1 to 100
 // create a variable to represent the current number
-let x = 1
+let x = 1;
 // create a variable to represent the current total
-let total = 0
+let total = 0;
 // write a while loop that sums the numbers from 1 to 100
 while (x <= 100) {
-    total = total + x
-    x += 1
+    total = total + x;
+    x += 1;
 }
-console.log(total)
+console.log(total);
---
+++ Section02/2.3.8-ForLoops.js
@@ -0,0 +1,6 @@
+// ? TO DO: create a for loop that TOTALS the numbers from 1 to 100
+let y = 0;
+for (let x = 1; x <= 100; x++) {
+    y += x;
+    console.log(y);
+}
